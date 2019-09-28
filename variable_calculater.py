8c163e4842b89a1c4dd0fa8791ea931537b3f07c
Benjamin <summ@kth.se>
2019-09-28 11:17:30 +0200

    added strategy score calculation as a function of l as well as a plot for strategy score vs l

--- variable_calculater.py
+++ variable_calculater.py
@@ -28,6 +28,16 @@ def get_average(trait_list):
 		average += float(trait_list[i])
 	return average/len(trait_list)
 
+def l_sum(trait_list,lower):
+	s = 0
+	for i in range(lower,len(trait_list)):
+		s += trait_list[i]
+	return s
+
+def mu_ss(d_apr,d_ir,d_s,N_ap,f_dm,f_cd):
+	strategy_score = 3.0/20 * (d_apr + d_ir) + 3.0/10 * (d_s + N_ap) + 1.0/20 * (f_dm + f_cd)
+	return strategy_score
+
 team_names = ["1-fc-koeln","borussia-dortmund","fc-schalke-04","borussia-moenchengladbach","fortuna-duesseldorf","sc-paderborn-07",
 "tsg-1899-hoffenheim","1-fc-union-berlin","eintracht-frankfurt","1-fsv-mainz-05","fc-augsburg","hertha-bsc","sport-club-freiburg",
 "vfl-wolfsburg","bayer-04-leverkusen","fc-bayern-muenchen","rb-leipzig","sv-werder-bremen"]
@@ -36,80 +46,101 @@ for team_name in team_names:
 
 	with open(team_name + ".csv") as csv_file:
     		csv_reader = csv.reader(csv_file, delimiter=',')
-    		d_ap = []
+    		d_apr = []
     		d_ir = []
     		d_s = []
     		N_ap = []
-    		#number of matches dominated by the team itself
-    		N_dm = 0
-    		#number of countered dominations
-    		N_cd = 0
+    		#an indicator for N_dm containing 1 if the match was dominated and 0 otherwise
+    		I_dm = []
+    		#an indicator for N_cd containing 1 if the match as counter-dominated and 0 otherwise
+    		I_cd = []
     		row_count = 0
     		for row in csv_reader:
     			if row_count != 0:
-    				d_ap.append(inversion(np.exp((-float(row[33]) + float(row[27]))/100)))
+    				d_apr.append(inversion(np.exp((-float(row[33]) + float(row[27]))/100)))
     				d_ir.append(np.exp(-float(row[27])))
     				d_s.append(np.exp(-float(row[28])))
     				N_ap.append(inversion(np.exp(-float(row[18])/float(row[19]))))
     				f_pp = float(row[23])
     				if f_pp > 65.0:
-    					N_dm += 1
+    					I_dm.append(1)
+    				else:
+    					I_dm.append(0)
     				if f_pp < (100.0 - 65.0) and float(row[0]) > float(row[1]):
-    					N_cd += 1
+    					I_cd.append(1)
+    				else:
+    					I_cd.append(0)
     			row_count += 1
 		csv_file.close()
 
-	num_of_matches = float(row_count-1)
-	average_d_ap = get_average(d_ap)
-	average_d_ir = get_average(d_ir)
-	average_d_s = get_average(d_s)
-	average_N_ap = get_average(N_ap)
-	f_dm = N_dm/num_of_matches
-	f_cd = inversion(N_cd/num_of_matches)
-	strategy_score = 3.0/20 * (average_d_ap + average_d_ir) + 3.0/10 * (average_d_s + average_N_ap) + 1.0/20 * (f_dm + f_cd)
-	
-	d_ap_rob = []
+	num_of_matches = row_count-1	
+	d_apr_rob = []
 	d_ir_rob = []
 	d_s_rob = []
 	N_ap_rob = []
-	for l in range(1,len(d_ir)):
-		d_ap_mean = l_average(d_ap,l)
-		d_ap_std = l_std(d_ap,l,d_ap_mean)
-		d_ap_index = d_ap_std/d_ap_mean
-		d_ap_rob.append(np.exp(-d_ap_index))
+	d_apr_averages = []
+	d_ir_averages = []
+	d_s_averages = []
+	N_ap_averages = []
+	f_dms = []
+	f_cds = []
+	strategy_scores = []
+	for l in range(1, num_of_matches):
+		d_apr_mean = l_average(d_apr,l)
+		d_apr_averages.append(d_apr_mean)
+		d_apr_std = l_std(d_apr,l,d_apr_mean)
+		d_apr_index = d_apr_std/d_apr_mean
+		d_apr_rob.append(np.exp(-d_apr_index))
 		d_ir_mean = l_average(d_ir,l)
+		d_ir_averages.append(d_ir_mean)
 		d_ir_std = l_std(d_ir,l,d_ir_mean)
 		d_ir_index = d_ir_std/d_ir_mean
 		d_ir_rob.append(np.exp(-d_ir_index))
 		d_s_mean = l_average(d_s,l)
+		d_s_averages.append(d_s_mean)
 		d_s_std = l_std(d_s,l,d_s_mean)
 		d_s_index = d_s_std/d_s_mean
 		d_s_rob.append(np.exp(-d_s_index))
 		N_ap_mean = l_average(N_ap,l)
+		N_ap_averages.append(N_ap_mean)
 		N_ap_std = l_std(N_ap,l,N_ap_mean)
 		N_ap_index = N_ap_std/N_ap_mean
 		N_ap_rob.append(np.exp(-N_ap_index))
+		f_dm = l_average(I_dm,l)
+		f_dms.append(f_dm)
+		f_cd = inversion(l_average(I_cd,l))
+		f_cds.append(f_cd)
+		strategy_scores.append(mu_ss(d_apr_mean,d_ir_mean,d_s_mean,N_ap_mean,f_dm,f_cd))
 	f = plt.figure()
 	plt.subplot(221)
-	plt.title("Robsustness of d_ap")
-	plt.plot(range(1,len(d_ap_rob)+1),d_ap_rob)
+	plt.title("Robustness of d_apr")
+	plt.plot(range(1,len(d_apr_rob)+1),d_apr_rob)
 	plt.subplot(222)
-	plt.title("Robsustness of d_ir")
+	plt.title("Robustness of d_ir")
 	plt.plot(range(1,len(d_ir_rob)+1),d_ir_rob)
 	plt.subplot(223)
-	plt.title("Robsustness of d_s")
+	plt.title("Robustness of d_s")
 	plt.plot(range(1,len(d_s_rob)+1),d_s_rob)
 	plt.subplot(224)
-	plt.title("Robsustness of N_ap")
+	plt.title("Robustness of N_ap")
 	plt.plot(range(1,len(N_ap_rob)+1),N_ap_rob)
 	plt.subplots_adjust(hspace = 0.5)
 	f.savefig(team_name + ".pdf")
 	plt.close()
+	fss = plt.figure()
+	plt.title("mu_ss")
+	plt.plot(range(1,len(d_ir_rob)+1),strategy_scores)
+	plt.xlabel("l")
+	plt.ylabel("mu_ss")
+	fss.savefig(team_name + "_mu_ss.pdf")
+	plt.close()
+
 
 	file_name = team_name + "_vars_and_measures.csv"
 	with open(file_name, mode="w") as team_file:
 			writer = csv.writer(team_file, delimiter=",")
 			writer.writerow(["d_apr","d_ir","d_s","N_ap","f_dm","f_cd","ss"])
-			writer.writerow([average_d_ap,average_d_ir,average_d_s,average_N_ap,f_dm,f_cd,strategy_score])
+			for i in range(num_of_matches-1):
+				writer.writerow([d_apr_averages[i],d_ir_averages[i],d_s_averages[i],N_ap_averages[i],f_dms[i],f_cds[i],strategy_scores[i]])
 			team_file.close()
 
